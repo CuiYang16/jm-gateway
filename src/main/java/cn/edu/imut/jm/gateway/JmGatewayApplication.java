0ae286b23092dcaa67bf876e46a988ab78506dc7
CuiYang16 <cuiyangmail@163.com>
2019-04-10 09:46:09 +0800

    first

---
+++ src/main/java/cn/edu/imut/jm/gateway/JmGatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.imut.jm.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/imut/jm/gateway/JmGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.edu.imut.jm.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JmGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
